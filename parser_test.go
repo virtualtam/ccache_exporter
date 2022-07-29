15b17636453067035d055d67653d779668c62ea8
VirtualTam <virtualtam@flibidi.net>
2022-07-29 18:35:21 +0200

    parser: add testdata for Debian 9, 10 and Ubuntu 18.04 (ccache < 3.7)
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- parser_test.go
+++ parser_test.go
@@ -6,157 +6,374 @@ package ccache
 
 import (
 	"errors"
+	"fmt"
 	"io/ioutil"
+	"path/filepath"
 	"testing"
 
 	"github.com/alecthomas/units"
 )
 
+type testCase struct {
+	tname         string
+	inputFilename string
+	want          *Statistics
+	wantErr       error
+}
+
+type testSession struct {
+	osAndVersion     string
+	osAndVersionCode string
+	ccacheVersion    string
+	testCases        []testCase
+}
+
 func TestParseReference(t *testing.T) {
-	cases := []struct {
-		tname     string
-		inputFile string
-		want      *Statistics
-		wantErr   error
-	}{
-		// ccache 3.4.3 on Arch Linux
+
+	sessions := []testSession{
 		{
-			tname:     "3.4.3 empty cache",
-			inputFile: "testdata/arch-3.4.3/empty",
-			want: &Statistics{
-				CacheDirectory:          "/home/virtualtam/.ccache",
-				PrimaryConfig:           "/home/virtualtam/.ccache/ccache.conf",
-				SecondaryConfigReadonly: "/etc/ccache.conf",
-				CacheSize:               "0.0 kB",
-				MaxCacheSize:            "15.0 GB",
-				MaxCacheSizeBytes:       units.MetricBytes(15000000000),
-			},
-		},
-		{
-			tname:     "3.4.3 first build",
-			inputFile: "testdata/arch-3.4.3/firstbuild",
-			want: &Statistics{
-				CacheDirectory:           "/home/virtualtam/.ccache",
-				PrimaryConfig:            "/home/virtualtam/.ccache/ccache.conf",
-				SecondaryConfigReadonly:  "/etc/ccache.conf",
-				CacheMiss:                116,
-				CalledForLink:            14,
-				CalledForPreprocessing:   85,
-				UnsupportedCodeDirective: 2,
-				NoInputFile:              29,
-				FilesInCache:             361,
-				CacheSize:                "6.4 MB",
-				CacheSizeBytes:           units.MetricBytes(6400000),
-				MaxCacheSize:             "15.0 GB",
-				MaxCacheSizeBytes:        units.MetricBytes(15000000000),
-			},
-		},
-		{
-			tname:     "3.4.3 second build",
-			inputFile: "testdata/arch-3.4.3/secondbuild",
-			want: &Statistics{
-				CacheDirectory:           "/home/virtualtam/.ccache",
-				PrimaryConfig:            "/home/virtualtam/.ccache/ccache.conf",
-				SecondaryConfigReadonly:  "/etc/ccache.conf",
-				CacheHitDirect:           73,
-				CacheHitPreprocessed:     4,
-				CacheMiss:                207,
-				CacheHitRate:             27.11,
-				CacheHitRatio:            0.2711,
-				CalledForLink:            28,
-				CalledForPreprocessing:   170,
-				UnsupportedCodeDirective: 4,
-				NoInputFile:              58,
-				FilesInCache:             639,
-				CacheSize:                "12.1 MB",
-				CacheSizeBytes:           units.MetricBytes(12100000),
-				MaxCacheSize:             "15.0 GB",
-				MaxCacheSizeBytes:        units.MetricBytes(15000000000),
+			osAndVersion:     "Arch Linux",
+			osAndVersionCode: "arch",
+			ccacheVersion:    "3.4.3",
+
+			testCases: []testCase{
+				{
+					tname:         "empty cache",
+					inputFilename: "empty",
+					want: &Statistics{
+						CacheDirectory:          "/home/virtualtam/.ccache",
+						PrimaryConfig:           "/home/virtualtam/.ccache/ccache.conf",
+						SecondaryConfigReadonly: "/etc/ccache.conf",
+						CacheSize:               "0.0 kB",
+						MaxCacheSize:            "15.0 GB",
+						MaxCacheSizeBytes:       units.MetricBytes(15000000000),
+					},
+				},
+				{
+					tname:         "first build",
+					inputFilename: "firstbuild",
+					want: &Statistics{
+						CacheDirectory:           "/home/virtualtam/.ccache",
+						PrimaryConfig:            "/home/virtualtam/.ccache/ccache.conf",
+						SecondaryConfigReadonly:  "/etc/ccache.conf",
+						CacheMiss:                116,
+						CalledForLink:            14,
+						CalledForPreprocessing:   85,
+						UnsupportedCodeDirective: 2,
+						NoInputFile:              29,
+						FilesInCache:             361,
+						CacheSize:                "6.4 MB",
+						CacheSizeBytes:           units.MetricBytes(6400000),
+						MaxCacheSize:             "15.0 GB",
+						MaxCacheSizeBytes:        units.MetricBytes(15000000000),
+					},
+				},
+				{
+					tname:         "second build",
+					inputFilename: "secondbuild",
+					want: &Statistics{
+						CacheDirectory:           "/home/virtualtam/.ccache",
+						PrimaryConfig:            "/home/virtualtam/.ccache/ccache.conf",
+						SecondaryConfigReadonly:  "/etc/ccache.conf",
+						CacheHitDirect:           73,
+						CacheHitPreprocessed:     4,
+						CacheMiss:                207,
+						CacheHitRate:             27.11,
+						CacheHitRatio:            0.2711,
+						CalledForLink:            28,
+						CalledForPreprocessing:   170,
+						UnsupportedCodeDirective: 4,
+						NoInputFile:              58,
+						FilesInCache:             639,
+						CacheSize:                "12.1 MB",
+						CacheSizeBytes:           units.MetricBytes(12100000),
+						MaxCacheSize:             "15.0 GB",
+						MaxCacheSizeBytes:        units.MetricBytes(15000000000),
+					},
+				},
 			},
 		},
 
-		// ccache 3.5 on Arch Linux
 		{
-			tname:     "3.5 empty cache",
-			inputFile: "testdata/arch-3.5/empty",
-			want: &Statistics{
-				CacheDirectory:          "/home/virtualtam/.ccache",
-				PrimaryConfig:           "/home/virtualtam/.ccache/ccache.conf",
-				SecondaryConfigReadonly: "/etc/ccache.conf",
-				CacheSize:               "0.0 kB",
-				MaxCacheSize:            "5.0 GB",
-				MaxCacheSizeBytes:       units.MetricBytes(5000000000),
+			osAndVersion:     "Arch Linux",
+			osAndVersionCode: "arch",
+			ccacheVersion:    "3.5",
+
+			testCases: []testCase{
+				{
+					tname:         "empty cache",
+					inputFilename: "empty",
+					want: &Statistics{
+						CacheDirectory:          "/home/virtualtam/.ccache",
+						PrimaryConfig:           "/home/virtualtam/.ccache/ccache.conf",
+						SecondaryConfigReadonly: "/etc/ccache.conf",
+						CacheSize:               "0.0 kB",
+						MaxCacheSize:            "5.0 GB",
+						MaxCacheSizeBytes:       units.MetricBytes(5000000000),
+					},
+				},
+				{
+					tname:         "first build",
+					inputFilename: "firstbuild",
+					want: &Statistics{
+						CacheDirectory:          "/home/virtualtam/.ccache",
+						PrimaryConfig:           "/home/virtualtam/.ccache/ccache.conf",
+						SecondaryConfigReadonly: "/etc/ccache.conf",
+						CacheHitDirect:          1,
+						CacheHitPreprocessed:    15,
+						CacheMiss:               342,
+						CacheHitRate:            4.47,
+						CacheHitRatio:           0.0447,
+						CalledForLink:           14,
+						CalledForPreprocessing:  1,
+						FilesInCache:            867,
+						CacheSize:               "44.5 MB",
+						CacheSizeBytes:          units.MetricBytes(44500000),
+						MaxCacheSize:            "5.0 GB",
+						MaxCacheSizeBytes:       units.MetricBytes(5000000000),
+					},
+				},
+				{
+					tname:         "second build",
+					inputFilename: "secondbuild",
+					want: &Statistics{
+						CacheDirectory:          "/home/virtualtam/.ccache",
+						PrimaryConfig:           "/home/virtualtam/.ccache/ccache.conf",
+						SecondaryConfigReadonly: "/etc/ccache.conf",
+						CacheHitDirect:          349,
+						CacheHitPreprocessed:    10,
+						CacheMiss:               28,
+						CacheHitRate:            92.76,
+						CacheHitRatio:           0.9276000000000001,
+						CalledForLink:           14,
+						CalledForPreprocessing:  1,
+						FilesInCache:            943,
+						CacheSize:               "46.7 MB",
+						CacheSizeBytes:          units.MetricBytes(46700000),
+						MaxCacheSize:            "5.0 GB",
+						MaxCacheSizeBytes:       units.MetricBytes(5000000000),
+					},
+				},
+			},
+		},
+
+		{
+			osAndVersion:     "Debian 9",
+			osAndVersionCode: "debian-9",
+			ccacheVersion:    "3.3.4",
+
+			testCases: []testCase{
+				{
+					tname:         "empty cache",
+					inputFilename: "empty",
+					want: &Statistics{
+						CacheDirectory:          "/home/cached/.ccache",
+						PrimaryConfig:           "/home/cached/.ccache/ccache.conf",
+						SecondaryConfigReadonly: "/etc/ccache.conf",
+						CacheSize:               "0.0 kB",
+						MaxCacheSize:            "5.0 GB",
+						MaxCacheSizeBytes:       units.MetricBytes(5000000000),
+					},
+				},
+				{
+					tname:         "first build",
+					inputFilename: "firstbuild",
+					want: &Statistics{
+						CacheDirectory:           "/home/cached/.ccache",
+						PrimaryConfig:            "/home/cached/.ccache/ccache.conf",
+						SecondaryConfigReadonly:  "/etc/ccache.conf",
+						CacheMiss:                52,
+						CalledForLink:            1,
+						CalledForPreprocessing:   11,
+						NoInputFile:              7,
+						UnsupportedCodeDirective: 1,
+						FilesInCache:             103,
+						CacheSize:                "1.2 MB",
+						CacheSizeBytes:           units.MetricBytes(1200000),
+						MaxCacheSize:             "5.0 GB",
+						MaxCacheSizeBytes:        units.MetricBytes(5000000000),
+					},
+				},
+				{
+					tname:         "second build",
+					inputFilename: "secondbuild",
+					want: &Statistics{
+						CacheDirectory:           "/home/cached/.ccache",
+						PrimaryConfig:            "/home/cached/.ccache/ccache.conf",
+						SecondaryConfigReadonly:  "/etc/ccache.conf",
+						CacheHitDirect:           50,
+						CacheHitPreprocessed:     2,
+						CacheMiss:                52,
+						CacheHitRate:             50.0,
+						CacheHitRatio:            0.5,
+						CalledForLink:            2,
+						CalledForPreprocessing:   22,
+						NoInputFile:              14,
+						UnsupportedCodeDirective: 2,
+						FilesInCache:             103,
+						CacheSize:                "1.2 MB",
+						CacheSizeBytes:           units.MetricBytes(1200000),
+						MaxCacheSize:             "5.0 GB",
+						MaxCacheSizeBytes:        units.MetricBytes(5000000000),
+					},
+				},
 			},
 		},
 		{
-			tname:     "3.5 first build",
-			inputFile: "testdata/arch-3.5/firstbuild",
-			want: &Statistics{
-				CacheDirectory:          "/home/virtualtam/.ccache",
-				PrimaryConfig:           "/home/virtualtam/.ccache/ccache.conf",
-				SecondaryConfigReadonly: "/etc/ccache.conf",
-				CacheHitDirect:          1,
-				CacheHitPreprocessed:    15,
-				CacheMiss:               342,
-				CacheHitRate:            4.47,
-				CacheHitRatio:           0.0447,
-				CalledForLink:           14,
-				CalledForPreprocessing:  1,
-				FilesInCache:            867,
-				CacheSize:               "44.5 MB",
-				CacheSizeBytes:          units.MetricBytes(44500000),
-				MaxCacheSize:            "5.0 GB",
-				MaxCacheSizeBytes:       units.MetricBytes(5000000000),
+			osAndVersion:     "Debian 10",
+			osAndVersionCode: "debian-10",
+			ccacheVersion:    "3.6",
+
+			testCases: []testCase{
+				{
+					tname:         "empty cache",
+					inputFilename: "empty",
+					want: &Statistics{
+						CacheDirectory:          "/home/cached/.ccache",
+						PrimaryConfig:           "/home/cached/.ccache/ccache.conf",
+						SecondaryConfigReadonly: "/etc/ccache.conf",
+						CacheSize:               "0.0 kB",
+						MaxCacheSize:            "5.0 GB",
+						MaxCacheSizeBytes:       units.MetricBytes(5000000000),
+					},
+				},
+				{
+					tname:         "first build",
+					inputFilename: "firstbuild",
+					want: &Statistics{
+						CacheDirectory:          "/home/cached/.ccache",
+						PrimaryConfig:           "/home/cached/.ccache/ccache.conf",
+						SecondaryConfigReadonly: "/etc/ccache.conf",
+						CacheMiss:               57,
+						CalledForLink:           1,
+						CalledForPreprocessing:  11,
+						NoInputFile:             4,
+						FilesInCache:            110,
+						CacheSize:               "1.7 MB",
+						CacheSizeBytes:          units.MetricBytes(1700000),
+						MaxCacheSize:            "5.0 GB",
+						MaxCacheSizeBytes:       units.MetricBytes(5000000000),
+					},
+				},
+				{
+					tname:         "second build",
+					inputFilename: "secondbuild",
+					want: &Statistics{
+						CacheDirectory:          "/home/cached/.ccache",
+						PrimaryConfig:           "/home/cached/.ccache/ccache.conf",
+						SecondaryConfigReadonly: "/etc/ccache.conf",
+						CacheHitDirect:          52,
+						CacheHitPreprocessed:    5,
+						CacheMiss:               57,
+						CacheHitRate:            50.0,
+						CacheHitRatio:           0.5,
+						CalledForLink:           2,
+						CalledForPreprocessing:  22,
+						NoInputFile:             8,
+						FilesInCache:            110,
+						CacheSize:               "1.7 MB",
+						CacheSizeBytes:          units.MetricBytes(1700000),
+						MaxCacheSize:            "5.0 GB",
+						MaxCacheSizeBytes:       units.MetricBytes(5000000000),
+					},
+				},
 			},
 		},
+
 		{
-			tname:     "3.5 second build",
-			inputFile: "testdata/arch-3.5/secondbuild",
-			want: &Statistics{
-				CacheDirectory:          "/home/virtualtam/.ccache",
-				PrimaryConfig:           "/home/virtualtam/.ccache/ccache.conf",
-				SecondaryConfigReadonly: "/etc/ccache.conf",
-				CacheHitDirect:          349,
-				CacheHitPreprocessed:    10,
-				CacheMiss:               28,
-				CacheHitRate:            92.76,
-				CacheHitRatio:           0.9276000000000001,
-				CalledForLink:           14,
-				CalledForPreprocessing:  1,
-				FilesInCache:            943,
-				CacheSize:               "46.7 MB",
-				CacheSizeBytes:          units.MetricBytes(46700000),
-				MaxCacheSize:            "5.0 GB",
-				MaxCacheSizeBytes:       units.MetricBytes(5000000000),
+			osAndVersion:     "Ubuntu 18.04",
+			osAndVersionCode: "ubuntu-18.04",
+			ccacheVersion:    "3.4.1",
+
+			testCases: []testCase{
+				{
+					tname:         "empty cache",
+					inputFilename: "empty",
+					want: &Statistics{
+						CacheDirectory:          "/home/cached/.ccache",
+						PrimaryConfig:           "/home/cached/.ccache/ccache.conf",
+						SecondaryConfigReadonly: "/etc/ccache.conf",
+						CacheSize:               "0.0 kB",
+						MaxCacheSize:            "5.0 GB",
+						MaxCacheSizeBytes:       units.MetricBytes(5000000000),
+					},
+				},
+				{
+					tname:         "first build",
+					inputFilename: "firstbuild",
+					want: &Statistics{
+						CacheDirectory:          "/home/cached/.ccache",
+						PrimaryConfig:           "/home/cached/.ccache/ccache.conf",
+						SecondaryConfigReadonly: "/etc/ccache.conf",
+						CacheMiss:               53,
+						CalledForLink:           1,
+						CalledForPreprocessing:  11,
+						NoInputFile:             4,
+						FilesInCache:            104,
+						CacheSize:               "1.6 MB",
+						CacheSizeBytes:          units.MetricBytes(1600000),
+						MaxCacheSize:            "5.0 GB",
+						MaxCacheSizeBytes:       units.MetricBytes(5000000000),
+					},
+				},
+				{
+					tname:         "second build",
+					inputFilename: "secondbuild",
+					want: &Statistics{
+						CacheDirectory:          "/home/cached/.ccache",
+						PrimaryConfig:           "/home/cached/.ccache/ccache.conf",
+						SecondaryConfigReadonly: "/etc/ccache.conf",
+						CacheHitDirect:          50,
+						CacheHitPreprocessed:    3,
+						CacheMiss:               53,
+						CacheHitRate:            50.0,
+						CacheHitRatio:           0.5,
+						CalledForLink:           2,
+						CalledForPreprocessing:  22,
+						NoInputFile:             8,
+						FilesInCache:            104,
+						CacheSize:               "1.6 MB",
+						CacheSizeBytes:          units.MetricBytes(1600000),
+						MaxCacheSize:            "5.0 GB",
+						MaxCacheSizeBytes:       units.MetricBytes(5000000000),
+					},
+				},
 			},
 		},
 	}
 
-	for _, tc := range cases {
-		t.Run(tc.tname, func(t *testing.T) {
-			input, err := ioutil.ReadFile(tc.inputFile)
-			if err != nil {
-				t.Fatalf("failed to open test input: %q", err)
-			}
-
-			s, err := Parse(string(input))
-
-			if tc.wantErr != nil {
-				if err == nil {
-					t.Fatal("expected an error, got none")
-				} else if err.Error() != tc.wantErr.Error() {
-					t.Fatalf("want error %q, got %q", tc.wantErr, err)
+	for _, ts := range sessions {
+		for _, tc := range ts.testCases {
+			t.Run(fmt.Sprintf("ccache %s on %s (%s)", ts.ccacheVersion, ts.osAndVersion, tc.tname), func(t *testing.T) {
+				inputFilepath := filepath.Join(
+					"testdata",
+					fmt.Sprintf("%s-%s", ts.osAndVersionCode, ts.ccacheVersion),
+					tc.inputFilename,
+				)
+				input, err := ioutil.ReadFile(inputFilepath)
+				if err != nil {
+					t.Fatalf("failed to open test input: %q", err)
 				}
 
-				return
-			}
+				s, err := Parse(string(input))
 
-			if err != nil {
-				t.Fatalf("expected no error, got %q", err)
-			}
+				if tc.wantErr != nil {
+					if err == nil {
+						t.Fatal("expected an error, got none")
+					} else if err.Error() != tc.wantErr.Error() {
+						t.Fatalf("want error %q, got %q", tc.wantErr, err)
+					}
 
-			assertStatisticsEqual(t, s, tc.want)
-		})
+					return
+				}
+
+				if err != nil {
+					t.Fatalf("expected no error, got %q", err)
+				}
+
+				assertStatisticsEqual(t, s, tc.want)
+			})
+		}
 	}
 }
 
